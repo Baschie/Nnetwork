819d116967d4ed0d9e5a04246d375fc33b20eb46
erkei <cuprogrammer@gmail.com>
2024-07-25 20:59:08 +0330

    Fix small bug in FILL macro

--- matrix.h
+++ matrix.h
@@ -2,7 +2,7 @@
 #include <string.h>
 
 #define ENTRY(mat, i, j) mat->entries[mat->col * i + j]
-#define FILL(mat, ...) mat->memcpy(mat->entries, (double []) {__VA_ARGS__}, \
+#define FILL(mat, ...) memcpy(mat->entries, (double []) {__VA_ARGS__}, \
 	sizeof((double []) {__VA_ARGS__})/sizeof(double))
 
 typedef struct {
