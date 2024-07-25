7d973d170d1e94b54bba37fc258c612d00b35b41
erkei <cuprogrammer@gmail.com>
2024-07-25 08:59:47 +0330

    Create header file for matrix lib

---
+++ matrix.h
@@ -0,0 +1,11 @@
+#pragma once
+
+typedef struct {
+	int row;
+	int col;
+	double *entries;
+} Matrix;
+
+#define ENTRY(mat, i, j) mat->entries[mat->col * i + j]
+#define FILL(mat, ...) mat->memcpy(mat->entries, (double []) {__VA_ARGS__}, \
+	sizeof((double []) {__VA_ARGS__})/sizeof(double))
